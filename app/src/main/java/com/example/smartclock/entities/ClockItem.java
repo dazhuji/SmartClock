efc2bafefb5a8b3a79cb167df993b7ddf3e02c83
dazhuji <2358364656@qq.com>
2019-11-15 15:14:00 +0800

    例行提交

---
+++ app/src/main/java/com/example/smartclock/entities/ClockItem.java
@@ -0,0 +1,13 @@
+package com.example.smartclock.entities;
+
+public class ClockItem {
+    private byte hour;
+    private int minute;
+    private boolean enable;
+    private boolean autoRepeat;
+    private boolean shakeWhileRinging;
+    private byte repeatDay;
+    private String song;
+    private String description;
+
+}
