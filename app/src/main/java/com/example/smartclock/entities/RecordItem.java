ef1a008b74f868027a707e6a765d3477c1ed4abe
dazhuji <2358364656@qq.com>
2019-11-19 09:34:02 +0800

    1）增加了早起记录文件 2）增加了早起记录实体类 3）增加了早起记录文件读写工具类

---
+++ app/src/main/java/com/example/smartclock/entities/RecordItem.java
@@ -0,0 +1,59 @@
+package com.example.smartclock.entities;
+
+import java.util.Date;
+
+public class RecordItem {
+    private Date date;
+    private int hour;
+    private int minute;
+    private int percent;
+    private int days;
+
+    public RecordItem(Date date, int hour, int minute, int percent, int days) {
+        this.date = date;
+        this.hour = hour;
+        this.minute = minute;
+        this.percent = percent;
+        this.days = days;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public int getPercent() {
+        return percent;
+    }
+
+    public void setPercent(int percent) {
+        this.percent = percent;
+    }
+
+    public int getDays() {
+        return days;
+    }
+
+    public void setDays(int days) {
+        this.days = days;
+    }
+}
