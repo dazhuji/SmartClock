0870d534ccde4e5368499232682de7c18a30325d
zhaoyifengf <532023385@qq.com>
2019-11-22 22:21:22 +0800

    修改了很多东西，就不一一列举了。

--- app/src/main/java/com/example/smartclock/fragments/AlarmClocksShowListFragment.java
+++ app/src/main/java/com/example/smartclock/fragments/AlarmClocksShowListFragment.java
@@ -4,6 +4,7 @@ package com.example.smartclock.fragments;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -18,6 +19,7 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.smartclock.MainActivity;
 import com.example.smartclock.R;
 import com.example.smartclock.adapters.AlarmClockListAdapter;
 import com.example.smartclock.pojo.AlarmClockItem;
@@ -28,9 +30,11 @@ import java.util.List;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AlarmClocksShowListFragment extends Fragment {
+public class AlarmClocksShowListFragment extends Fragment implements AlarmClockListAdapter.SwitchClickedListener {
 
     private View view;
+    private AlarmClockListAdapter adapter;
+    private ListView listView;
     private List<AlarmClockItem> list;
 
     public AlarmClocksShowListFragment() {
@@ -39,38 +43,52 @@ public class AlarmClocksShowListFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_alarm_clocks_show_list, container, false);
+        adapter = new AlarmClockListAdapter();
+        adapter.setSwitchClickedListener(this);
+        listView = (ListView)view.findViewById(R.id.AlarmClockList);
+        listView.setAdapter(adapter);
         final AlarmClocksShowListViewModel viewModel =
                 ViewModelProviders.of(getActivity()).get(AlarmClocksShowListViewModel.class);
-
         //Create an observer that updates the UI when the observed data source has been changed
         final Observer<List<AlarmClockItem>> listObserver = new Observer<List<AlarmClockItem>>() {
             @Override
             public void onChanged(@Nullable final List<AlarmClockItem> newList) {
                 // Update the UI
                 list = newList;
-                AlarmClockListAdapter adapter = new AlarmClockListAdapter(newList);
-                ListView list = (ListView)view.findViewById(R.id.AlarmClockList);
-                list.setAdapter(adapter);
+                adapter.setData(list);
+                adapter.notifyDataSetChanged();
             }
         };
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        AlertDialog.Builder alterDiaglog = new AlertDialog.Builder(getContext());
 
-        ((ListView)view.findViewById(R.id.AlarmClockList)).setOnItemClickListener(
-                new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getContext(), "列表项被点击了", Toast.LENGTH_SHORT)
-                        .show();
-            }
-        });
+                        alterDiaglog.setView(R.layout.time_change_dialog);//加载进去
+                        AlertDialog dialog = alterDiaglog.create();
+                       
+                        dialog.show();
+
+                    }
+                });
 
         viewModel.getAlarmClockList().observe(this, listObserver);
-
         return view;
     }
 
+    @Override
+    public void itemClicked(boolean b,int position) {
+        list.get(position).setEnable(b);
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
 }
