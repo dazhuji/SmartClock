9b765452170fc22541b1303855e1c7ed64f64588
zhuzhengdong <379865499@qq.com>
2019-11-28 15:26:02 +0800

    实现历史统计界面

--- app/src/main/java/com/example/smartclock/utils/RecordUtil.java
+++ app/src/main/java/com/example/smartclock/utils/RecordUtil.java
@@ -2,21 +2,21 @@ package com.example.smartclock.utils;
 
 import android.content.Context;
 
-import com.example.smartclock.pojo.NewsRecordItem;
+import com.example.smartclock.pojo.RecordDate;
 
 import java.util.List;
 
-public class NewsRecordUtil {
+public class RecordUtil {
 
-    private List<NewsRecordItem> list;
+    private List<RecordDate> list;
 
-    public List<NewsRecordItem> getNewsRecord(Context context)
+    public List<RecordDate> getNewsRecord(Context context)
     {
         //TODO 完成从文件中返回记录方法的编写
         return null;
     }
 
-    public void saveNewsRecord(List<NewsRecordItem> list)
+    public void saveNewsRecord(List<RecordDate> list)
     {
         //TODO 完成将记录写回文件方法的编写
     }
